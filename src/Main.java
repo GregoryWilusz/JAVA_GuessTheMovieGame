451b43f1db53972d1c1780bfec353c06b917ab38
Greg <wilusz.gregory@gmail.com>
2018-07-17 17:55:13 +0200

    Improved rendering guessed wrong letters

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Character> wrongLetters = new ArrayList<>();
+        StringBuilder wrongLetters = new StringBuilder();
+        int wrongLettersCounter = 0;
         String guessingLetter;
         boolean hasWon = false;
 
@@ -20,7 +21,7 @@ public class Main {
                 hasWon = true;
             } else {
                 System.out.println("You are guessing: " + encodedMovie);
-                System.out.println("You guessed (" + wrongLetters.size() + ") wrong letters: " + wrongLetters.toString());
+                System.out.println("You guessed (" + wrongLettersCounter + ") wrong letters: " + wrongLetters);
                 System.out.println("You have: " + attemptsLeft + " attempts left.");
                 System.out.println("Guess a letter: ");
 
@@ -31,12 +32,13 @@ public class Main {
                     attemptsLeft++;
                     encodedMovie = movies.decodeMovie(pickedMovie, encodedMovie, guessingLetter);
                 } else {
-                    wrongLetters.add(guessingLetter.charAt(0));
+                    wrongLettersCounter++;
+                    wrongLetters.append(guessingLetter.charAt(0)).append(" ");
                 }
             }
         }
         if (hasWon) {
-            System.out.println("You won! The correct answer is: " + encodedMovie);
+            System.out.println("You won! The answer is: " + encodedMovie);
         } else {
             System.out.println("Game over. The correct answer is: " + pickedMovie);
         }
